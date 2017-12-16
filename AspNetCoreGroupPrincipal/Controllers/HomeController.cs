ffca815dee99655e102b726a35fb20d9cc4e9ec1
Benjamin Hysell <benh@viewpointusa.com>
2017-12-15 23:49:51 -0500

    Initial

--- AspNetCoreGroupPrincipal/Controllers/HomeController.cs
+++ AspNetCoreGroupPrincipal/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.DirectoryServices.AccountManagement;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -11,6 +12,14 @@ namespace AspNetCoreGroupPrincipal.Controllers
     {
         public IActionResult Index()
         {
+            using (var pc = new PrincipalContext(ContextType.Domain, "YOUR_DOMAIN_HERE", "USERNAME", "PW"))
+            {
+                SortedSet<string> ldapResults;
+                using (var gp = GroupPrincipal.FindByIdentity(pc, IdentityType.Name, "GROUP_TO_LOOKUP"))
+                {
+                    ldapResults = gp == null ? null : new SortedSet<string>(gp.GetMembers(true).Select(u => u.SamAccountName));
+                }
+            }
             return View();
         }
 
